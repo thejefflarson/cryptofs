7bb637bdaa9f6dd388aa560e5ee928fe9995dbd3
Jeff Larson <thejefflarson@gmail.com>
2014-01-20 02:15:08 -0500

    we can make files

--- src/cryptofs.c
+++ src/cryptofs.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <dirent.h>
 
 #include "lib/tweetnacl.h"
 #include <fuse.h>
@@ -14,14 +15,15 @@ static char *crypto_dir;
 static unsigned char key[crypto_secretbox_KEYBYTES];
 static size_t block_size = 1024;
 
-char * _crypto_path(char *crypto_path, const char *path){
-  return strndup(path, strnlen(path, PATH_MAX));
+char * _crypto_path(const char *path){
+  char *ret;
+  asprintf(&ret, "%s%s", crypto_dir, path);
+  return ret;
 }
 
 static int crypto_getattr(const char *path, struct stat *st){
-  char *cpath = _crypto_path(cpath, path);
+  char *cpath = _crypto_path(path);
   if(cpath == NULL) return -ENOMEM;
-
   int res = lstat(cpath, st);
   free(cpath);
 
@@ -31,8 +33,39 @@ static int crypto_getattr(const char *path, struct stat *st){
   return 0;
 }
 
+static int crypto_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+                    off_t offset, struct fuse_file_info *inf){
+  DIR *dp;
+  struct dirent *de;
+
+  (void) offset;
+  (void) inf;
+  char *cpath = _crypto_path(path);
+  if(cpath == NULL) return -ENOMEM;
+
+  dp = opendir(cpath);
+  if(dp == NULL) {
+    free(cpath);
+    return -errno;
+  }
+
+  while ((de = readdir(dp)) != NULL) {
+    struct stat st;
+    memset(&st, 0, sizeof(st));
+    st.st_ino = de->d_ino;
+    st.st_mode = de->d_type << 12;
+    int ret = filler(buf, de->d_name, &st, 0);
+    if(ret) break;
+  }
+
+  closedir(dp);
+  free(cpath);
+  return 0;
+}
+
 static int crypto_mknod(const char *path, mode_t mode, dev_t dev){
-  char *cpath = _crypto_path(cpath, path);
+  char *cpath = _crypto_path(path);
+
   if(cpath == NULL) return -ENOMEM;
 
   int res;
@@ -40,11 +73,12 @@ static int crypto_mknod(const char *path, mode_t mode, dev_t dev){
   if(S_ISFIFO(mode)) {
     res = mkfifo(cpath, mode);
   } else {
+    puts(cpath);
     res = mknod(cpath, mode, dev);
   }
 
   free(cpath);
-
+  printf("%i", res);
   if(res == -1)
     return -errno;
 
@@ -52,16 +86,32 @@ static int crypto_mknod(const char *path, mode_t mode, dev_t dev){
 }
 
 static int crypto_open(const char *path, struct fuse_file_info *inf){
-  char *cpath = _crypto_path(cpath, path);
+  char *cpath = _crypto_path(path);
   if(cpath == NULL) return -ENOMEM;
 
-  uint64_t fh = open(cpath, inf->flags);
+  int fh = open(cpath, inf->flags);
   free(cpath);
 
   if(fh == -1)
     return -errno;
 
   inf->fh = fh;
+
+  return 0;
+}
+
+static int crypto_create(const char *path, mode_t mode, struct fuse_file_info *inf){
+  char *cpath = _crypto_path(path);
+  if(cpath == NULL) return -ENOMEM;
+
+  int fh = open(cpath, inf->flags, mode);
+  free(cpath);
+
+  if(fh == -1)
+    return -errno;
+
+  inf->fh = fh;
+
   return 0;
 }
 
@@ -89,26 +139,28 @@ static int crypto_write(const char *path, const char *buf, size_t size,
 
 static struct fuse_operations crypto_ops = {
   .getattr  = crypto_getattr,
+  .readdir  = crypto_readdir,
   .mknod    = crypto_mknod,
   .open     = crypto_open,
+  .create   = crypto_create,
   .read     = crypto_read,
   .write    = crypto_write
 };
 
 int main(int argc, char *argv[]) {
+  umask(0);
   if(argc < 3) {
     printf("%i not enough arguments, usage: cryptofs <encdir> <mount>\n", argc);
-    fflush(stdout);
     return 1;
   }
   struct fuse_args args = FUSE_ARGS_INIT(0, NULL);
   for(int i = 0; i < argc; i++) {
     if (i == 1)
-      crypto_dir = argv[i];
+      crypto_dir = realpath(argv[i], NULL);
     else
       fuse_opt_add_arg(&args, argv[i]);
   }
-  crypto_dir = argv[1];
+  puts(crypto_dir);
   char *pw;
   pw = getpass("enter password: ");
   crypto_hash(key, (unsigned char *) pw, strnlen(pw, _PASSWORD_LEN));
