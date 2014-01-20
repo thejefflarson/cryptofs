c38763b3d2d5b7ef40ca8a97ab9fba06835a69ad
Jeff Larson <thejefflarson@gmail.com>
2014-01-20 00:42:52 -0500

    more stubs

--- src/cryptofs.c
+++ src/cryptofs.c
@@ -12,23 +12,49 @@
 
 static char *crypto_dir;
 static unsigned char key[crypto_secretbox_KEYBYTES];
+static size_t block_size = 1024;
 
-void _crypto_path(char *crypto_path, char *cpath){
-
+char * _crypto_path(char *crypto_path, const char *path){
+  return strndup(path, strnlen(path, PATH_MAX));
 }
 
-static int crypto_getattr(){
+static int crypto_getattr(const char *path, struct stat *st){
+  char *cpath = _crypto_path(cpath, path);
+  if(cpath == NULL) return -ENOMEM;
+
+  int res = lstat(cpath, st);
+  free(cpath);
+
+  if(res == -1)
+    return -errno;
+
   return 0;
 }
 
-static int crypto_mknod(const char * buf, mode_t mode, dev_t dev){
+static int crypto_mknod(const char *path, mode_t mode, dev_t dev){
+  char *cpath = _crypto_path(cpath, path);
+  if(cpath == NULL) return -ENOMEM;
+
+  int res;
+
+  if(S_ISFIFO(mode)) {
+    res = mkfifo(cpath, mode);
+  } else {
+    res = mknod(cpath, mode, dev);
+  }
+
+  free(cpath);
+
+  if(res == -1)
+    return -errno;
+
   return 0;
 }
 
 static int crypto_open(const char *path, struct fuse_file_info *inf){
-  char *cpath = malloc(strnlen(path, PATH_MAX));
+  char *cpath = _crypto_path(cpath, path);
   if(cpath == NULL) return -ENOMEM;
-  _crypto_path(cpath, path);
+
   uint64_t fh = open(cpath, inf->flags);
   free(cpath);
 
@@ -39,12 +65,26 @@ static int crypto_open(const char *path, struct fuse_file_info *inf){
   return 0;
 }
 
-static int crypto_read(){
-  return 0;
+static int crypto_read(const char *path, char *buf, size_t size,
+                       off_t off, struct fuse_file_info *inf){
+  (void) path;
+
+  int res = pread(inf->fh, buf, size, off);
+  if(res == -1)
+    return -errno;
+
+  return res;
 }
 
-static int crypto_write(){
-  return 0;
+static int crypto_write(const char *path, const char *buf, size_t size,
+                        off_t off, struct fuse_file_info *inf){
+  (void) path;
+
+  int res = pwrite(inf->fh, buf, size, off);
+  if(res == -1)
+    return -errno;
+
+  return res;
 }
 
 static struct fuse_operations crypto_ops = {
@@ -56,8 +96,9 @@ static struct fuse_operations crypto_ops = {
 };
 
 int main(int argc, char *argv[]) {
-  if(argc != 3) {
-    printf("not enough arguments, usage: cryptofs <encdir> <mount>");
+  if(argc < 3) {
+    printf("%i not enough arguments, usage: cryptofs <encdir> <mount>\n", argc);
+    fflush(stdout);
     return 1;
   }
   struct fuse_args args = FUSE_ARGS_INIT(0, NULL);
