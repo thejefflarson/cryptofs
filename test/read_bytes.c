6e47509dae22c4580bbdda46ff0bec4a5c2c02c4
Jeff Larson <thejefflarson@gmail.com>
2014-01-29 20:12:23 -0500

    better read_bytes

--- test/read_bytes.c
+++ test/read_bytes.c
@@ -5,17 +5,23 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
   if(argc != 4) return -1;
-  char b[atoi(argv[2])];
+  char b[atoi(argv[1])];
+  memset(b, 0, atoi(argv[1]));
   printf("%i %i %s\n", atoi(argv[1]), atoi(argv[2]), argv[3]);
 
   int fd = open(argv[3], O_RDONLY);
   if(fd == -1) return errno;
 
-  int res = pread(fd, b, atoi(argv[1]), atoi(argv[2]));
-  if(res == -1) return errno;
+  int res = pread(fd, b, atoi(argv[1]), atoi(argv[2]) );
+  if(res == -1) {
+    puts("error");
+    return errno;
+  }
+  b[atoi(argv[1]) - 1] = '\0';
 
   printf("%s\n", b);
 
